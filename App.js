2b1e0ff16dd2d9081721168140c474d1ae245047
AnthonyLandeon <145145578+Landeon09@users.noreply.github.com>
2024-11-02 00:02:22 +0000

    primer comit1

--- App.js
+++ App.js
@@ -54,7 +54,7 @@ function HomeTabs() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown:false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="ChooseInterestScreen" component={ChooseInterestScreen} />
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testMatch: ["**/__tests__/**/*.test.js", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
