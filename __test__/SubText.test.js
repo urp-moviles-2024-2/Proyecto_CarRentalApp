91dd1ffacbadee4a31d996ad0182cda593b2a7c3
AnthonyLandeon <145145578+Landeon09@users.noreply.github.com>
2024-11-08 07:16:52 +0000

    commit

--- __test__/SubText.test.js
+++ __test__/SubText.test.js
@@ -5,19 +5,12 @@ import SubText from '../components/SubText';
 describe('SubText Component', () => {
   it('should render correctly with children', () => {
     const { getByText } = render(<SubText>Este es un texto de prueba</SubText>);
-
-    // Verifica que el texto se renderiza correctamente
     const renderedText = getByText('Este es un texto de prueba');
     expect(renderedText).toBeTruthy();
   });
-
   it('should have the correct style', () => {
     const { getByTestId } = render(<SubText>Texto</SubText>);
-
-    // Verifica que el componente tiene el testID correcto
     const subTextContainer = getByTestId('subtext-container');
-
-    // Verifica que el componente tiene el estilo correcto
     expect(subTextContainer).toHaveStyle({
       justifyContent: 'center',
       flexDirection: 'row',
