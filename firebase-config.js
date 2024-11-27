529ab0d1b84ff31425f69da7fe906e3a8d86791b
AnthonyLandeon <145145578+Landeon09@users.noreply.github.com>
2024-11-27 22:19:30 +0000

    credentials ultima prueba

--- firebase-config.js
+++ firebase-config.js
@@ -17,3 +17,14 @@ export const auth = initializeAuth(app, {
 });
 const database=getDatabase(app);
 export {database};
+
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDsFGpcnrvzAJRUMs-sJcMliCxDUIlMXEk",
+//   authDomain: "loginappcar.firebaseapp.com",
+//   projectId: "loginappcar",
+//   storageBucket: "loginappcar.firebasestorage.app",
+//   messagingSenderId: "499695849448",
+//   appId: "1:499695849448:web:26762d96ffce761c1dde5d",
+//   measurementId: "G-XQ6BX87H8E"
+// };
