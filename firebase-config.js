e3b9ec5b0418f59ce454f74a5fd932f827eb8475
AnthonyLandeon <145145578+Landeon09@users.noreply.github.com>
2024-11-28 17:11:29 +0000

    credentials coment

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,25 @@ import {initializeApp} from 'firebase/app';
 import {initializeAuth, inMemoryPersistence} from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
+
 export const firebaseConfig = {
-  apiKey: "API_KEY_CREDENTIAL",
-  authDomain: "AUTH_DOMAIN_CREDENTIAL ",
-  projectId: "PROJECT_ID_CREDENTIAL",
-  storageBucket: "STORAGE_BUCKET_CREDENTIAL",
-  messagingSenderId: "MESSAGING_SENDER_ID_CREDENTIAL",
-  appId: "APP_ID_CREDENTIAL",
-  measurementId: "MEASUREMENT_ID_CREDENTIAL"
+  apiKey: "AIzaSyDsFGpcnrvzAJRUMs-sJcMliCxDUIlMXEk",
+  authDomain: "loginappcar.firebaseapp.com",
+  projectId: "loginappcar",
+  storageBucket: "loginappcar.firebasestorage.app",
+  messagingSenderId: "499695849448",
+  appId: "1:499695849448:web:26762d96ffce761c1dde5d",
+  measurementId: "G-XQ6BX87H8E"
 };
+// export const firebaseConfig = {
+//   apiKey: "API_KEY_CREDENTIAL",
+//   authDomain: "AUTH_DOMAIN_CREDENTIAL ",
+//   projectId: "PROJECT_ID_CREDENTIAL",
+//   storageBucket: "STORAGE_BUCKET_CREDENTIAL",
+//   messagingSenderId: "MESSAGING_SENDER_ID_CREDENTIAL",
+//   appId: "APP_ID_CREDENTIAL",
+//   measurementId: "MEASUREMENT_ID_CREDENTIAL"
+// };
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
   persistence: inMemoryPersistence,
@@ -19,12 +29,3 @@ const database=getDatabase(app);
 export {database};
 
 
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyDsFGpcnrvzAJRUMs-sJcMliCxDUIlMXEk",
-//   authDomain: "loginappcar.firebaseapp.com",
-//   projectId: "loginappcar",
-//   storageBucket: "loginappcar.firebasestorage.app",
-//   messagingSenderId: "499695849448",
-//   appId: "1:499695849448:web:26762d96ffce761c1dde5d",
-//   measurementId: "G-XQ6BX87H8E"
-// };
