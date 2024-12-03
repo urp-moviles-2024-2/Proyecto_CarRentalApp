3d6e0f5421dab1938b60eae3276ea5c9ad23589d
carlosrivasesp <113138594+carlosrivasesp@users.noreply.github.com>
2024-12-03 02:14:39 +0000

    prueba actualizada

--- __test__/PrimaryButton.test.js
+++ __test__/PrimaryButton.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import PrimaryButton from '../components/PrimaryButton';
+import { GLOBAL_STYLES } from "../constants/styles";
 
 describe('PrimaryButton Component', () => {
   test('debe aplicar el estilo correspondiente cuando se presiona el botón', () => {
@@ -13,7 +14,7 @@ describe('PrimaryButton Component', () => {
     const button = getByTestId('primary-button');
 
     // Verifica que el estilo inicial tenga el color de fondo correcto
-    expect(button).toHaveStyle({ backgroundColor: "#9acd32" });
+    expect(button).toHaveStyle({ backgroundColor: GLOBAL_STYLES.colors.colorverdeprincipal });
 
     // Simula un clic en el botón
     fireEvent.press(button);
